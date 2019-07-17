0c0c1cb0309f02888bacf90760304adaf6ef30dd
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2019-07-18 02:34:09 +0530

    feat(world_cordinates): switched to world coordinate projection; Added boundary condition to restrict particles from escaping

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/imgproc.hpp>
 
-#define MAX_X 640
-#define MAX_Y 480
+#define WORLD_MAX_X 600
+#define WORLD_MAX_Y 600
 
 #define SOFTENING 1
-#define NUM_PARTICLES 1500
+#define NUM_PARTICLES 1000
 
 using namespace std;
 using namespace cv;
@@ -27,9 +27,10 @@ typedef struct{float x,y,mass ; float vx,vy;} Body;
 void randomlyAllocate(Body *data,int n)
 {
 for (int i=0;i<n;i++){
-    data[i].x =( std::rand() % ( MAX_X + 1 ));
-    data[i].y =( std::rand() % ( MAX_Y + 1 ));
-    data[i].mass = ( std::rand() % (3)) + 1;
+    data[i].x =( std::rand() % ( 100 ));
+    data[i].y =( std::rand() % ( 100 ));
+    //data[i].mass = ( std::rand() % (3)) + 1;
+    data[i].mass = 1;
     }
 }
 
@@ -37,15 +38,15 @@ void calcForce(Body *p,float dt, int numBodies)
 {
 #pragma omp parallel for schedule(dynamic)
 for (int i=0;i<numBodies;i++){
-    float fx=0.0f;
-    float fy=0.0f;
+    double fx=0.0f;
+    double fy=0.0f;
 
     for(int j=0;j<numBodies;j++){
-        float dx= p[j].x - p[i].x;
-        float dy= p[j].y - p[i].y;
-        float sqdist=dx*dx + dy*dy +SOFTENING;
-        float InvDist = 1.0f/(sqrtf(sqdist));
-        float InvDist3= InvDist*InvDist*InvDist;
+        double dx= p[j].x - p[i].x;
+        double dy= p[j].y - p[i].y;
+        double sqdist=dx*dx + dy*dy +SOFTENING;
+        double InvDist = 1.0f/(sqrtf(sqdist));
+        double InvDist3= InvDist*InvDist*InvDist;
         fx+= (dx * InvDist3 * p[j].mass) / p[i].mass ;
         fy+= (dy * InvDist3 * p[j].mass) / p[i].mass ;
     }
@@ -53,20 +54,25 @@ for (int i=0;i<numBodies;i++){
     p[i].vx+=dt*fx;
     p[i].vy+=dt*fy;
 
+    if(p[i].x < 0 || p[i].x > 100 ){ p[i].vx*=-1;}
+    if(p[i].y < 0 || p[i].y > 100 ){ p[i].vy*=-1;}
+
     }
 }
 
 int main()
     {
+    double scale_x = WORLD_MAX_X / 100;
+    double scale_y = WORLD_MAX_Y / 100;
 
-    Mat disp(Mat(MAX_Y,MAX_X, CV_8UC3));
+    Mat disp(Mat(WORLD_MAX_X, WORLD_MAX_Y, CV_8UC3));
     disp=Scalar(128,128,128);
     int nBodies= NUM_PARTICLES;
-    int dt=1;
+    double dt = 0.1;
     int nIter=100000;
 
     int bytes = nBodies*sizeof(Body);
-    float *buf = (float*)malloc(bytes);
+    double *buf = (double*)malloc(bytes);
     Body *p = (Body*)buf;
 
     randomlyAllocate(p,nBodies);
@@ -81,10 +87,11 @@ int main()
         for(int i=0;i<nBodies;i++){
                 p[i].x+=(int)p[i].vx*dt;
                 p[i].y+=(int)p[i].vy*dt;
-                circle(disp,Point(int(p[i].x),int(p[i].y)),p[i].mass,Scalar(255,255,255),-1);
+
+                circle(disp,Point(round(p[i].x * scale_x),round(p[i].y * scale_x)),p[i].mass,Scalar(255,255,255),-1);
 
         }
-        putText(disp,"Epoch: "+to_string(iter),Point(MAX_X - 200,30),FONT_HERSHEY_COMPLEX,0.5,Scalar(255,255,255),1,LINE_8,false);
+        putText(disp,"Epoch: "+to_string(iter),Point(WORLD_MAX_X/2 * scale_x,30),FONT_HERSHEY_COMPLEX,0.5,Scalar(255,255,255),1,LINE_8,false);
         imshow("Nbody",disp);
 
         if( waitKey(30)==0)
